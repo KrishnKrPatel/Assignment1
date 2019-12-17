30babc8c81095cda7283d7ca61d0a20ba3cfd207
Krishn Kumar Patel <patelkrishnk1213@gmail.com>
2019-12-17 12:28:55 +0530

    new filed

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/krishn/3EB680EEB680A84B/django_projects/assignment1/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
